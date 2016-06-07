9e8fe55890743469ed8ad4679de5d7b089c7066d
evanyu <evanyu@vip.qq.com>
2016-06-07 10:55:33 +0800

    修改Glide演示代码

--- ImageLoader/src/main/java/com/evan/imageloader/GlideDemo.java
+++ ImageLoader/src/main/java/com/evan/imageloader/GlideDemo.java
@@ -1,15 +1,17 @@
 package com.evan.imageloader;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -22,6 +24,15 @@ public class GlideDemo extends AppCompatActivity {
     @BindView(R.id.imageView)
     ImageView mImageView;
 
+    // Glide加载图像时使用的目标对象,会在图像加载完成后执行回调方法
+    // (如果使用这种方式加载图像,控件大小不确定,默认加载全尺寸图像,可以在构造方法中自己手动设置)
+    private SimpleTarget<Bitmap> target = new SimpleTarget<Bitmap>(250, 250) {
+        @Override
+        public void onResourceReady(Bitmap bm, GlideAnimation<? super Bitmap> glideAnimation) {
+            mImageView.setImageBitmap(bm);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,38 +42,45 @@ public class GlideDemo extends AppCompatActivity {
 
     @OnClick(R.id.btn_load_img)
     public void onClick() {
-        // 网络图片
-        // Glide.with(this).load(Constant.url).into(mImageView);
-        // drawable资源
-        // Glide.with(this).load(R.drawable.bd_logo).into(mImageView);
-        // 资产目录中的图片
-        // Glide.with(this).load(readFromAssets("bd_logo.png")).into(mImageView);
-        // 加载GIF
-        // Glide.with(this).load("http://img2.imgtn.bdimg.com/it/u=1608871163,3223894477&fm=21&gp=0.jpg").into(mImageView);
-        Glide.with(this).load(R.raw.splash_gif).into(mImageView);
+        Glide.with(this) // picasso只能是Conetext类型,而Glide支持Context,Activity,Fragment等类型
+                .load("https://www.baidu.com/img/bd_logo1.png") // 网络图片
+                //.load(R.drawable.bd_logo) // drawable资源
+                //.load(readFromAssets("bd_logo.png")) // 资产目录中的图片
+                //.load(R.raw.splash_gif) // gif图片(这行代码存在问题,加载很慢,待解决)
+                //.load(resIdToUri(this, R.drawable.bd_logo)) // uri对象(uri不必从资源总获取,可以是任何uri)
+                //.load(Uri.fromFile(new File(path))) // 加载视频(只能是本地视频)
+                //.asGif() // 判断url是否是一个gif图,如果不是的话就加载失败(即使这个url访问的是一个完整的图片)
+                .asBitmap() // 如果是已将gif图则只会加载git的第一祯
+                .placeholder(R.mipmap.ic_launcher) // 占位图(加载时显示)
+                .error(R.drawable.error) // 展位图(加载失败时显示)
+                //.crossFade(2000) // 淡入淡出效果(默认300ms)
+                .dontAnimate() // 去除淡入淡出效果
+                .override(200, 300) // 在图片显示之间重新设置图片的尺寸
+                //.centerCrop() // 将图片显示在控件冲,并裁剪超出部分
+                .fitCenter() // 将图片完全显示在空间中,保证图片的边界一定小于等于控件的边界
+                //.skipMemoryCache(true) // 设置是否跳过内存缓存(默认false)
+                //.diskCacheStrategy(DiskCacheStrategy.SOURCE) // 调整磁盘缓存策略(参数是一个枚举值)
+                //.priority(Priority.HIGH) // 优先级(规则类似于线程的优先级,并不会完全遵守)
+                //.thumbnail(0.1f) // 缩略图
+                //.into(mImageView);
+                //.into(ViewTarget) // 用于将图片显示在自定义View中
+                .into(target); // 设置加载完后的回调对象,可在内部的回调方法中处理加载到的图像(如果是Bitmap类型通常需要asBitmap()进行检测)
 
-        // 其他设置
-        // 占位图
-        // .placeholder(R.drawable.placeholder)
-        // 加载图
-        // .error(R.drawable.imagenotfound)
-        // 既缓存全尺寸又缓存其他尺寸(Glide默认会缓存图片,即使是同一张图片的不同尺寸也会缓存)
-        // .diskCacheStrategy(DiskCacheStrategy.ALL)
-        // 重新调整图片大小
-        // .override(300, 200);
-        // 去除过度动画
-        // .dontAnimate()
 
-        // .centerCrop();
-        // .transform(new CircleTransform(context))
+        /*
+         * 磁盘缓存的参数
+         *
+         * 参数是以下枚举值之一:
+         * NONE     禁用磁盘缓存
+         * SOURCE   只缓存原来的全分辨率的图像
+         * RESULT   只缓存最终显示的图像
+         * ALL      缓存所有版本的图像(默认)
+         */
 
     }
 
-    @OnClick(R.id.btn_load_video)
-    public void loadVideo() {
-        String path = Environment.getExternalStorageDirectory()+"/xpg.mp4";
-        Glide.with(this).load(Uri.fromFile(new File(path))).into(mImageView);
-       // Glide.with(this).load(Uri.fromFile(new File(path))).into();
+    private Uri resIdToUri(Context context, int resId) {
+        return Uri.parse("android.resource://" + context.getPackageName() + "/" + resId);
     }
 
     private byte[] readFromAssets(String fileName) {
@@ -73,7 +91,7 @@ public class GlideDemo extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if(is != null) {
+            if (is != null) {
                 try {
                     is.close();
                     is = null;
--- imageloader/src/main/java/com/evan/imageloader/MainActivity.java
+++ imageloader/src/main/java/com/evan/imageloader/MainActivity.java
@@ -1,14 +1,12 @@
 package com.evan.imageloader;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-
-import com.bumptech.glide.Glide;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,10 +15,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @BindView(R.id.listView)
     ListView mListView;
-    @BindView(R.id.imageView)
-    ImageView mImageView;
 
-    private String[] items = {"加载网络图片", "加载本地gif动图"};
+    private ActivityInfo[] activityData = {
+            new ActivityInfo("glide", GlideDemo.class)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        ArrayAdapter<ActivityInfo> adapter = new ArrayAdapter<ActivityInfo>(this, android.R.layout.simple_list_item_1, activityData);
         mListView.setAdapter(adapter);
 
         mListView.setOnItemClickListener(this);
@@ -37,16 +35,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        switch (position) {
-            case 0: // 加载网络图片
-                String url = "https://www.baidu.com/img/bd_logo1.png";
-                Glide.with(this).load(url).into(mImageView);
-                break;
-            case 1: // 加载本地gif动图
-
-                break;
-            default:
-                break;
-        }
+        startActivity(new Intent(this, activityData[position].clazz));
     }
 }
